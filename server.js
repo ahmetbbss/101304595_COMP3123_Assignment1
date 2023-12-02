d6160bd3dd0dfba2ab3aad7abeeefd4c8092856b
ahmetbbss <104052830+ahmetbbss@users.noreply.github.com>
2023-12-02 14:39:14 -0500

    proxy changed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const employeeRoute = require('./routes/employee_route');
 const cors = require('cors');
 app.use(cors(
   {
-    origin: 'https://656b837b824ffc787e24d436--capable-creponne-de69d4.netlify.app',
+    origin: 'https://101304595-comp-3123-assignment2.vercel.app',
     credentials: true
   }
 ));
